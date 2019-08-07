63072f61fa154b7e2c1956023450811009367a86
Andrew <kingharvest@gmail.com>
2019-08-07 13:26:01 -0400

    Moved comic panel code to comic.js for dynamic loading

--- comic.js
+++ comic.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
 
+    const comicList = ["<div class=\"comic-title\" id=\"comic-title\">\
+                        <h2>#1 <span class=\"bolder\">Giant Coat Sale</span></h2>\
+                        </div>\
+                        <div class=\"comic-img\" id=\"comic-img\">\
+                        <a href=\"01-giant-coat-sale.html\"><img src=\"images/comics/01.png\" alt=\"Greetings From Space #1: Giant Coat Sale\"></a>\
+                        </div>"
+                    ];
 
+    function loadComic (n) {
+        $(".comic-body").prepend(comicList[n]);
+    };
 
-
-
-
-
-
-
-
-
+loadComic(0);
 
 
 });
